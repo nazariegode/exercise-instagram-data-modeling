c48940f848a79e0db4cd41a572b2ee2edbd8f104
David Nazariego Ulacio <dnazariego@gmail.com>
2024-07-12 15:27:12 +0000

    instagram sqlalchemy

--- src/models.py
+++ src/models.py
@@ -1,12 +1,19 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+import enum
+from sqlalchemy import Column, ForeignKey, Integer, String, Text, Enum
 from sqlalchemy.orm import relationship, declarative_base
 from sqlalchemy import create_engine
 from eralchemy2 import render_er
 
 Base = declarative_base()
 
+class MediaType(enum.Enum):
+    IMAGE = 'image'
+    VIDEO = 'video'
+
+
+"""
 class Person(Base):
     __tablename__ = 'person'
     # Here we define columns for the table person
@@ -27,6 +34,40 @@ class Address(Base):
 
     def to_dict(self):
         return {}
+"""
+
+class User (Base):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True)
+    username = Column(String(250), nullable=False)
+    firstname = Column(String(250), nullable=False)
+    lastname = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False, unique=True)
+
+class Follower(Base):
+    __tablename__ = 'followers'
+    user_from_id = Column(Integer, ForeignKey('users.id'), primary_key=True)
+    user_to_id = Column(Integer, ForeignKey('users.id'), primary_key=True)
+
+class Post(Base):
+    __tablename__ = 'posts'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('users.id'), nullable=False)
+
+class Comment(Base):
+    __tablename__= 'comments'
+    id = Column(Integer, primary_key=True)
+    comment_text = Column(Text, nullable=False)
+    author_id = Column(Integer, ForeignKey('users.id'), nullable=False)
+    post_id = Column(Integer, ForeignKey('posts.id'), nullable=False)
+
+class Media(Base):
+    __tablename__ = 'medias'
+    id = Column(Integer, primary_key=True)
+    type = Column(Enum(MediaType), nullable=False, default=MediaType.IMAGE)
+    url = Column(String, nullable=False)
+    post_id = Column(Integer, ForeignKey('posts.id'), nullable=False)
+
 
 ## Draw from SQLAlchemy base
 try:
